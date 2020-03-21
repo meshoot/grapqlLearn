baef75c511b5e05b3b6b4801de227d8a6365bda4
mm@tdsgn.ru <mm@tdsgn.ru>
2020-03-21 14:16:04 +0700

    remove node_modules from repository

--- schema/schema.js
+++ schema/schema.js
@@ -5,7 +5,8 @@ const {
     GraphQLSchema, 
     GraphQLID,
     GraphQLInt,
-    GraphQLList
+    GraphQLList,
+    GraphQLNonNull
 } = graphql;
 
 const Movies = require('../models/movie');
@@ -15,8 +16,8 @@ const MovieType = new GraphQLObjectType({
     name: 'Movie',
     fields: () => ({
         id: { type: GraphQLString },
-        name: { type: GraphQLString },
-        genre: { type: GraphQLString },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        genre: { type: new GraphQLNonNull(GraphQLString) },
         director: {
             type: DirectorType,
             resolve(parent, args) {
@@ -30,8 +31,8 @@ const DirectorType = new GraphQLObjectType({
     name: 'Director',
     fields: () => ({
         id: { type: GraphQLString },
-        name: { type: GraphQLString },
-        age: { type: GraphQLInt },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
         movies: {
             type: new GraphQLList(MovieType),
             resolve(parent, args) {
@@ -77,6 +78,90 @@ const Query = new GraphQLObjectType({
     }
 });
 
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addDirector: {
+            type: DirectorType,
+            args: {
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: new GraphQLNonNull(GraphQLInt) }
+            },
+            resolve(parrent, args) {
+                const director = new Directors({
+                    name: args.name,
+                    age: args.age
+                });
+
+                return director.save();
+            }
+        },
+        addMovie: {
+            type: MovieType,
+            args: {
+                name: { type: GraphQLString },
+                genre: { type: GraphQLString },
+                directorId: { type: GraphQLID }
+            },
+            resolve(parrent, args) {
+                const movie = new Movies({
+                    name: args.name,
+                    genre: args.genre,
+                    directorId: args.directorId
+                });
+
+                return movie.save();
+            }
+        },
+        deleteDirector: {
+            type: DirectorType,
+            args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+            resolve(parent, args) {
+                return Directors.findByIdAndRemove(args.id)
+            }
+        },
+        deleteMovie: {
+            type: MovieType,
+            args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+            resolve(parent, args) {
+                return Movies.findByIdAndRemove(args.id)
+            }
+        },
+        updateDirector: {
+            type: DirectorType,
+            args: {
+                id: { type: GraphQLID },
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: new GraphQLNonNull(GraphQLInt) }
+            },
+            resolve(parrent, args) {
+                return Directors.findByIdAndUpdate(
+                    args.id,
+                    {$set: {name: args.name, age: args.age}},
+                    {new: true}
+                )
+            }
+        },
+        updateMovie: {
+            type: MovieType,
+            args: {
+                id: { type: GraphQLID },
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                genre: { type: new GraphQLNonNull(GraphQLString) },
+                directorId: { type: GraphQLID }
+            },
+            resolve(parrent, args) {
+                return Movies.findByIdAndUpdate(
+                    args.id,
+                    { $set: {name: args.name, genre: args.genre, directorId: args.directorId }},
+                    { new: true }
+                )
+            }
+        }
+    }
+});
+
 module.exports = new GraphQLSchema({
     query: Query,
+    mutation: Mutation
 });
